984e083c9741384797301e4687254bb6d2ecf360
Sanduni Sansala <karunathilakasanduni73@gmail.com>
2024-12-03 21:45:08 +0530

    configure tailwindcss

---
+++ src/pages/MyBooking.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyBooking:React.FC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MyBooking
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+export default { 
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary-color":"#007E85",
+        "secondary-color":"#6EAB36"
+      }
+    },
   },
   plugins: [],
 }
