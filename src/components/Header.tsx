c6863b953510e74b5ca7c3337a9a6c0580399c2d
Sanduni Sansala <karunathilakasanduni73@gmail.com>
2024-12-06 23:47:55 +0530

    Create Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
 
-const Header:React.FC = () => {
+const Header: React.FC = () => {
   return (
-    <div>
-      
+    <div className="w-full flex justify-center items-center">
+      <div className="flex flex-row container bg-background-color px-3 py-5">
+        <div className="basis-1/4">MedSync</div>
+        <div className="basis-2/4 flex gap-5 font-semibold cursor-pointer ">
+          <div
+            className="hover:underline hover:text-[#007E85]"
+            onClick={() => console.log("Home")}
+          >
+            Home
+          </div>
+          <div
+            className="hover:underline hover:text-[#007E85]"
+            onClick={() => console.log("Docter Channel")}
+          >
+            Docter Channel
+          </div>
+          <div
+            className="hover:underline hover:text-[#007E85]"
+            onClick={() => console.log("About")}
+          >
+            About
+          </div>
+          <div
+            className="hover:underline hover:text-[#007E85]"
+            onClick={() => console.log("Contact Us")}
+          >
+            Contact Us
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default { 
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors:{
-        "primary-color":"#007E85",
-        "secondary-color":"#6EAB36"
-      }
+      colors: {
+        "primary-color": "#007E85",
+        "secondary-color": "#6EAB36",
+        "background-color": "#ECECEC",
+      },
     },
   },
   plugins: [],
-}
-
+};
