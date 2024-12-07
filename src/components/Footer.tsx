dbfed4e34e2465f99ca14c3da740340c2953f655
Sanduni Sansala <karunathilakasanduni73@gmail.com>
2024-12-07 20:00:25 +0530

    Card

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Footer:React.FC = () => {
+const Footer: React.FC = () => {
   return (
-    <div>
-      
+    <div className="bg-primary-color text-white">
+      <div>
+        <h3>Location</h3>
+      </div>
+      <div>
+        <h3></h3>
+      </div>
+      <div>
+        <h3></h3>
+      </div>
+      <div className="text-align: center; margin-top: 20px; font-size: 12px;">
+    © DIGITAL HEALTH PVT LTD. ALL RIGHTS RESERVED.<br/>
+    Technology Partner: <span className="color: #fff; font-weight: bold;">mediwave</span>
+  </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
+import Card from '../components/Card';
 
 const About:React.FC = () => {
   return (
     <div>
+      <Header/>
+      <div className="w-full flex justify-center">
+        <div className=" container grid grid-cols-3 gap-5 p-5">
+          <Card/><Card/><Card/><Card/>
+        </div>
+      </div>
       
     </div>
   )
