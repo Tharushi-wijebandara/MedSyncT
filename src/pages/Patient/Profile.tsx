220765dacf9d6ab82ac56e7b13e2e406cb6f8857
SanduniSansala <karunathilakasanduni73@gmail.com>
2025-01-17 03:57:37 +0530

    Edit profile

---
+++ src/pages/Patient/Profile.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from '../../components/Footer'
+
+export const Profile:React.FC = () => {
+  return (
+    <div>
+         
+      <div></div>
+      <Footer/>
+      
+    </div>
+  )
+}
