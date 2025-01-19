3156da9fb6ebe0274d7725bd495634a2850e0ad7
Tharushi-wijebandara <tharushiwijebandara715@gmail.com>
2025-01-19 10:49:02 +0530

    change Dhome

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import "./App.css";
-import Home from "./pages/patient/Home";
-import About from "./pages/patient/About";
-import DoctorChannel from "./pages/patient/DoctorChannel";
-import Channel from "./pages/patient/Channel";
-import Book from "./pages/patient/Book";
-import Contact from "./pages/patient/Contact";
-import Loging from "./pages/patient/Loging";
-import Payment from "./pages/patient/Payment";
-import DHome from "./pages/doctor/DHome";
-import Form from "./pages/doctor/Form";
-import DLoging from "./pages/doctor/DLoging";
-import Register from "./pages/doctor/Register";
-import Schedule from "./pages/doctor/Schedule";
-import AHome from "./pages/admin/AHome";
-import NotFoundPage from "./pages/patient/NotFoundPage";
-import { Profile } from "./pages/patient/Profile";
+import Home from "./pages/Patient/Home";
+import About from "./pages/Patient/About"; 
+import DoctorChannel from "./pages/Patient/DoctorChannel"
+import Channel from "./pages/Patient/Channel";
+import Book from "./pages/Patient/Book";
+import Contact from "./pages/Patient/Contact";
+import Loging from "./pages/Patient/Loging";
+import Payment from "./pages/Patient/Payment";
+import DHome from "./pages/Doctor/DHome";
+import Form from "./pages/Doctor/Form";
+import DLoging from "./pages/Doctor/DLoging"
+import Register from "./pages/Doctor/Register";
+import Schedule from "./pages/Doctor/Schedule";
+import AHome from "./pages/Admin/AHome";
+import NotFoundPage from "./pages/Patient/NotFoundPage";
+import {Profile} from "./pages/Patient/Profile"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
